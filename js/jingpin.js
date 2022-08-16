d56db58e980b88e868ed366c21f1312258669842
精品小屋 <111035888+jingpinxiaowu@users.noreply.github.com>
2022-08-16 19:56:11 +0800

    Add files via upload

---
+++ js/jingpin.js
@@ -0,0 +1,60 @@
+export function detatest() {
+	var jingpinxiezhen = [
+		{
+			title: "喵小吉",
+			title_url: "https://s1.328888.xyz/2022/08/11/6OfwB.jpg",
+			sequence: "cover1",
+			total: "雨中漫步（74P/1.23G）",
+			time:"2022-8-12",
+			imgurl1:"https://s1.328888.xyz/2022/08/11/6OfwB.jpg",
+			imgurl2:"https://s1.328888.xyz/2022/08/11/6OfwB.jpg",
+			imgurl3:"https://s1.328888.xyz/2022/08/11/6OfwB.jpg",
+			id:"1"
+		},
+		{
+			title: "巧克力",
+			title_url: "https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			sequence: "cover2",
+			total: "1",
+			time:"2022-8-11",
+			imgurl1:"https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			imgurl2:"https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			imgurl3:"https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			id:"2"
+		},
+		{
+			title: "巧克力",
+			title_url: "https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			sequence: "cover2",
+			total: "1",
+			time:"2022-8-11",
+			imgurl1:"https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			imgurl2:"https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			imgurl3:"https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			id:"2"
+		},
+		{
+			title: "巧克力",
+			title_url: "https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			sequence: "cover2",
+			total: "1",
+			time:"2022-8-11",
+			imgurl1:"https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			imgurl2:"https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			imgurl3:"https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			id:"2"
+		},
+		{
+			title: "巧克力",
+			title_url: "https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			sequence: "cover2",
+			total: "1",
+			time:"2022-8-11",
+			imgurl1:"https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			imgurl2:"https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			imgurl3:"https://s1.328888.xyz/2022/08/12/6rUpX.jpg",
+			id:"2"
+		}
+	];
+	return jingpinxiezhen;
+}
---
+++ js/youzhi.js
@@ -0,0 +1,70 @@
+export function detatest() {
+	var youzhixiezhen = [
+		{
+			title: "萌芽儿",
+			title_url: "https://s1.328888.xyz/2022/08/14/Txp0C.jpg",
+			content:"免费（30P/208M）",
+			sequence: "cover1",
+			total: "爱宕泳装",
+			time:"2022-8-12",
+			imgurl1:"https://s1.328888.xyz/2022/08/14/Txp0C.jpg",
+			imgurl2:"https://s1.328888.xyz/2022/08/14/TxRtP.jpg",
+			imgurl3:"https://s1.328888.xyz/2022/08/14/TxWBX.jpg",
+			id:"0",
+			price: "0"
+		},
+		{
+			title: "王雨纯",
+			title_url: "https://s1.328888.xyz/2022/08/14/TJ9AN.jpg",
+			content:"免费（62P/332M）",
+			sequence: "cover4",
+			total: "vol.292",
+			time:"2022-8-12",
+			imgurl1:"https://s1.328888.xyz/2022/08/14/TJ9AN.jpg",
+			imgurl2:"https://s1.328888.xyz/2022/08/14/TJc4y.jpg",
+			imgurl3:"https://s1.328888.xyz/2022/08/14/TJp8w.jpg",
+			id:"1",
+			price: ""
+		},
+		{
+			title: "喵小吉",
+			title_url: "https://s1.328888.xyz/2022/08/11/6OfwB.jpg",
+			content:"免费",
+			sequence: "cover3",
+			total: "雨中漫步（74P/1.23G）",
+			time:"2022-8-12",
+			imgurl1:"https://s1.328888.xyz/2022/08/11/6OfwB.jpg",
+			imgurl2:"https://s1.328888.xyz/2022/08/11/6OfwB.jpg",
+			imgurl3:"https://s1.328888.xyz/2022/08/11/6OfwB.jpg",
+			id:"2",
+			price: ""
+		},
+		{
+			title: "喵小吉",
+			title_url: "https://s1.328888.xyz/2022/08/11/6OfwB.jpg",
+			content:"免费",
+			sequence: "cover2",
+			total: "雨中漫步（74P/1.23G）",
+			time:"2022-8-12",
+			imgurl1:"https://s1.328888.xyz/2022/08/11/6OfwB.jpg",
+			imgurl2:"https://s1.328888.xyz/2022/08/11/6OfwB.jpg",
+			imgurl3:"https://s1.328888.xyz/2022/08/11/6OfwB.jpg",
+			id:"3",
+			price: ""
+		},
+		{
+			title: "萌芽儿",
+			title_url: "https://s1.328888.xyz/2022/08/14/Txp0C.jpg",
+			content:"免费",
+			sequence: "cover1",
+			total: "爱宕泳装（30P/208M）",
+			time:"2022-8-12",
+			imgurl1:"https://s1.328888.xyz/2022/08/14/Txp0C.jpg",
+			imgurl2:"https://s1.328888.xyz/2022/08/14/TxRtP.jpg",
+			imgurl3:"https://s1.328888.xyz/2022/08/14/TxWBX.jpg",
+			id:"4",
+			price: ""
+		}
+	];
+	return youzhixiezhen;
+}
